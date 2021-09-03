b181aa449c84f8e3c95b98f64ae530f7b3c35b47
Matt Auer <mattauer@umich.edu>
2021-09-02 21:10:09 -0400

    updated address for redeploy

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,6 +3,6 @@ import ElectionFactory from './build/ElectionFactory.json';
 
 const instance = new web3.eth.Contract(
     ElectionFactory.abi,
-    "0xc05C8C9D0b88D403482DfDCE2e845e77F2ab059b"
+    "0x2505abF02FD3e1CeA36930cbc6846cdB7cb74E82"
 );
 export default instance;
